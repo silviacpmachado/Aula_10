0a4512afee2bc898ee94877b5208f3cb8e9a18cd
silviacpmachado <silviacpmachado@gmail.com>
2026-05-05 20:24:15 -0300

    inicializa

--- aula10/exemplo05.py
+++ aula10/exemplo05.py
@@ -1,18 +1,17 @@
 print(' === Cálculo de Produtividade ===')
 
 try:
-
    total_produzido = float(input('Valor total da venda:'))
    funcionarios = int(input('Total de Funcionários: '))
- 
-
    media_por_funcionario = total_produzido / funcionarios
-   print(f"Média por funcionário: {media_por_funcionario:.2f}")
-except (ValueError, TypeError):
-   print('O valor precisa ser numérico')
-except ZeroDivisionError:
-   print('Funcionário não pode ser Zero. ')
-
-# Se não der erro, execute o else.
+  
+except Exception as e:
+   print(f'Ops! Erro nos valores de entrada: {e}')
+except KeyboardInterrupt:
+   print('Operação cancelada pelo usuário')
 else:
    print(f"Média por funcionário: {media_por_funcionario:.2f}")
+
+#Executa sempre. Com erro ou não, o bloco finally sempre irá executar.
+finally:
+   print('Programa encerrado!')
